869dcd1a0353c43216bd846c0997b9dc86e13eed
Hung Thanh Do <hungdo2001vn@gmail.com>
2024-12-17 16:16:38 +0100

    Final version of train3.py and plots2.py
    Ready to train on colab

--- train3.py
+++ train3.py
@@ -14,7 +14,7 @@ from torchsummary import summary
 from data.dataloader import InfantVisionDataset
 from model import ResNet50
 from tqdm import tqdm
-from utils.plots2 import plot_losses, plot_metrics, plot_confusion_matrix
+from utils.plots2 import plot_losses, plot_metrics, plot_confusion_matrix, visualize
 from utils.metrics import precision_recall
 
 from torch.utils.tensorboard import SummaryWriter
@@ -22,7 +22,13 @@ from torch.utils.tensorboard import SummaryWriter
 
 def train(data, age_in_months, apply_blur, apply_contrast, weights, num_epochs, batch_size, lr, unfreeze_layer, resume_checkpoint, save_folder_name):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"\nUsing device: {device}\n")
+    # Check if CUDA is available
+    if torch.cuda.is_available():
+        for i in range(torch.cuda.device_count()):
+            print(f"Device {i}: {torch.cuda.get_device_name(i)}")
+            print(f"  - Device Memory: {torch.cuda.get_device_properties(i).total_memory / (1024**2):.2f} MB")
+    else:
+        print("No CUDA devices available. Using CPU.\n")
 
     # Define directories from data.yaml
     with open(data, 'r') as file:
@@ -146,6 +152,13 @@ def train(data, age_in_months, apply_blur, apply_contrast, weights, num_epochs, 
         save_folder = os.path.join("results", f"{save_folder_name}_{counter}")
     os.makedirs(save_folder, exist_ok=True)
 
+    if apply_blur or apply_contrast:
+        transform_type = "Blur + Contrast" if apply_blur and apply_contrast else "Blur" if apply_blur else "Contrast" if apply_contrast else "None"
+        save_path = f"results/{save_folder_name}/transform_visualization.png"
+        num_images = 5
+        print("Saving visualization of transformations for multi-dataloaders...\n")
+        visualize(train_loaders, age_in_months, transform_type, num_images, save_folder)
+
     # Initialize TensorBoard
     log_dir = os.path.join(save_folder, "tensorboard_logs")
     writer = SummaryWriter(log_dir)
@@ -185,7 +198,7 @@ def train(data, age_in_months, apply_blur, apply_contrast, weights, num_epochs, 
         if len(train_loaders) > 1:
             print(f"Stage {stage_idx + 1} - Age months {age_in_months[stage_idx]}:\n")
         for epoch in range(start_epoch, num_epochs * (stage_idx+1)):
-            print(f"EPOCH {epoch + 1}/{num_epochs * len(train_loaders)}")
+            print(f"EPOCH {epoch}/{num_epochs * len(train_loaders - 1)}")
             # Train phase
             model.train()
             running_loss = 0.0
@@ -258,12 +271,12 @@ def train(data, age_in_months, apply_blur, apply_contrast, weights, num_epochs, 
                 best_conf_matrix = conf_matrix
                 torch.save(model.state_dict(), best_ckpt_path)
 
-        all_train_losses.extend(train_losses)
-        all_val_losses.extend(val_losses)
-        all_val_precisions.extend(val_precisions)
-        all_val_recalls.extend(val_recalls)
-        all_class_precisions.extend(class_precisions)
-        all_class_recalls.extend(class_recalls)
+        # all_train_losses.extend(train_losses)
+        # all_val_losses.extend(val_losses)
+        # all_val_precisions.extend(val_precisions)
+        # all_val_recalls.extend(val_recalls)
+        # all_class_precisions.extend(class_precisions)
+        # all_class_recalls.extend(class_recalls)
         start_epoch += num_epochs   
 
 
@@ -276,15 +289,15 @@ def train(data, age_in_months, apply_blur, apply_contrast, weights, num_epochs, 
     print("Class\tPrecision\tRecall")
     for i in range(num_classes):
         class_name = class_names[i]
-        precision = all_class_precisions[best_epoch][i].item()
-        recall = all_class_recalls[best_epoch][i].item()
+        precision = class_precisions[best_epoch][i].item()
+        recall = class_recalls[best_epoch][i].item()
         print(f"{class_name}\t{precision:.2f}\t\t{recall:.2f}")
     print("-----------------------------\n")
 
     print(f"Saving results and plots to {save_folder}...\n")
     # Plot and save figures
-    plot_losses(all_train_losses, all_val_losses, save_folder, stage_boundaries)
-    plot_metrics(all_val_precisions, all_val_recalls, save_folder, stage_boundaries)
+    plot_losses(train_losses, val_losses, save_folder, stage_boundaries)
+    plot_metrics(val_precisions, val_recalls, save_folder, stage_boundaries)
     plot_confusion_matrix(best_conf_matrix, save_folder)
 
     writer.close()
